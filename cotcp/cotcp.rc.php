9be085e583d5a4bc0cd30dd3d92d5d7ae2400491
sodium carbonate <webitproff@gmail.com>
2025-07-07 21:13:45 +0300

    Update cotcp.rc.php
    Select2

--- cotcp/cotcp.rc.php
+++ cotcp/cotcp.rc.php
@@ -21,7 +21,10 @@ Resources::addFile('lib/bootstrap/css/bootstrap.min.css');
 Resources::addFile(Cot::$cfg['themes_dir'] . '/admin/' . Cot::$cfg['admintheme'] . '/assets/jquery/jquery.min.js');
 
 Resources::addFile(Cot::$cfg['themes_dir'] . '/admin/' . Cot::$cfg['admintheme'] . '/fontawesome/css/all.min.css', 'css', 200);
-
+Resources::addFile(Cot::$cfg['themes_dir'] . '/admin/' . Cot::$cfg['admintheme'] . '/assets/select2/select2.min.css');
+Resources::addFile(Cot::$cfg['themes_dir'] . '/admin/' . Cot::$cfg['admintheme'] . '/assets/select2/style-select2.css');
+Resources::addFile(Cot::$cfg['themes_dir'] . '/admin/' . Cot::$cfg['admintheme'] . '/assets/select2/select2.min.js');
+Resources::addFile(Cot::$cfg['themes_dir'] . '/admin/' . Cot::$cfg['admintheme'] . '/assets/select2/js-select2.js');
 Resources::addFile(Cot::$cfg['themes_dir'] . '/admin/' . Cot::$cfg['admintheme'] . '/css/modalbox.css');
 Resources::addFile(Cot::$cfg['themes_dir'] . '/admin/' . Cot::$cfg['admintheme'] . '/css/style.css');
 
@@ -36,9 +39,6 @@ Resources::addFile(Cot::$cfg['themes_dir'] . '/admin/' . Cot::$cfg['admintheme']
 Resources::linkFileFooter(Cot::$cfg['themes_dir'] . '/admin/' . Cot::$cfg['admintheme'] . '/assets/perfect-scrollbar/perfect-scrollbar.min.js');
 Resources::linkFileFooter(Cot::$cfg['themes_dir'] . '/admin/' . Cot::$cfg['admintheme'] . '/assets/perfect-scrollbar/js-perfect-scrollbar.js'); 
 
-// uncomment the lines below if you use this
-// Resources::addFile(Cot::$cfg['themes_dir'] . '/admin/' . Cot::$cfg['admintheme'] . '/assets/select2/select2.min.css');
-// Resources::addFile(Cot::$cfg['themes_dir'] . '/admin/' . Cot::$cfg['admintheme'] . '/assets/select2/select2.min.js');
 
 
 
@@ -49,8 +49,6 @@ if (Cot::$cfg['headrc_consolidate']) {
 }
 
 Resources::addFile(Cot::$cfg['themes_dir'] . '/admin/' . Cot::$cfg['admintheme'] . '/js/js.js');
-
-
 
 
 
